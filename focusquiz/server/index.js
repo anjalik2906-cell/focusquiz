f9df44608aff967064201826f46301fbd8aacbd7
Anjali <anjalik.2906@gmail.com>
2026-09-21 11:10:47 +0530

    modified:   focusquiz/server/index.js

--- focusquiz/server/index.js
+++ focusquiz/server/index.js
@@ -11,6 +11,7 @@ dotenv.config({ path: path.join(here, '..', '.env') });
 const { aiEnabled, generateQuiz } = await import('./quiz.js');
 const { saveSession, listSessions, storageKind } = await import('./store.js');
 const { upload, extractFile, youtubeToText } = await import('./extract.js');
+const { linkToText } = await import('./webpage.js');
 
 const app = express();
 app.use(cors());
